51c64ba8a9e85218c0f50b3034881debac3b4d8f
Vladislav4er <vk792@mail.ru>
2018-03-22 11:02:13 +0300

    Added Task aboy JavaDoc

--- part_001/src/main/java/ru.javavlad/Calculate.java
+++ part_001/src/main/java/ru.javavlad/Calculate.java
@@ -1,6 +1,19 @@
 package ru.javavlad;
 
+/**
+ * Print Hello World.
+ * @author Vlad
+ * @version 1.00
+ * @since 22.08.2018
+ */
+
 public class Calculate {
+
+/**
+ * Вывод строки Hello World.
+ * @param args - args
+ */
+
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 	}
---
+++ part_001/src/main/java/ru.javavlad/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Print Hello World.
+ * @author Vlad
+ * @version 1.00
+ * @since 22.08.2018
+ */
+
+package ru.javavlad;

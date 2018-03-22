8d65b3d3e40aed53243209f35589f87e2ca5138d
Vladislav4er <vk792@mail.ru>
2018-03-22 11:58:55 +0300

    Test for Calculate added

---
+++ part_001/src/test/java/ru/javavlad/CalculateTest.java
@@ -0,0 +1,31 @@
+package ru.javavlad;
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+* Test.
+*
+* @author Vlad
+* @version $Id$
+* @since 22.03.2018
+*/
+
+public class CalculateTest {
+
+/**
+* Test add.
+*/
+
+@Test
+public void whenAddOneToOneThenTwo() {
+	ByteArrayOutputStream out = new ByteArrayOutputStream();
+	System.setOut(new PrintStream(out));
+	Calculate.main(null);
+	assertThat(out.toString(), is(String.format("Hello World!%s", System.getProperty("line.separator")))
+	);
+	}
+}
---
+++ part_001/src/test/java/ru/javavlad/package-info.java
@@ -0,0 +1,9 @@
+/**
+* Test.
+*
+* @author Vlad
+* @version $Id$
+* @since 22.03.2018
+*/
+
+package ru.javavlad;
